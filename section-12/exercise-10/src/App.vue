4ddbd9f13c3824c2a99bb83511076e99a4faa860
Caue Queiroz <cauenqueiroz@gmail.com>
2019-01-09 18:21:00 -0200

    finish exercise 10

---
+++ section-12/exercise-10/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-birl:click="show">Click me!</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            show() {
+                alert('biiiirl');
+            }
+        },
+
+        directives: {
+            'birl': {
+                bind(el, binding) {
+                    el.addEventListener(binding.arg, binding.value);
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
